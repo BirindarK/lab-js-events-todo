8c10c9ccae7657e85084f95905934e2eb7a4acd0
BirindarK <birindar.bahia01@gmail.com>
2024-03-18 23:14:03 +0000

    Completed delete button

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -2,17 +2,37 @@ const toDoList = document.querySelector("#todo-form");
 const listedItem = document.querySelector("#list");
 const button = document.querySelector("#enter");
 
+toDoList.addEventListener("submit", (evt) =>{
+    evt.preventDefault();
+    //add items to the list
+
+    const newItem = document.createElement("li");
+    newItem.innerText = evt.target["new-todo"].value;
+    listedItem.appendChild(newItem);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete!";
+    listedItem.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", (evt) => {
+       listedItem.removeChild(newItem);
+       listedItem.removeChild(deleteButton);
+    })
+
+  
+})
 
 button.addEventListener("click", () => {
     console.log("item added to list");
 })
 
-toDoList.addEventListener("submit", (evt) =>{
-    evt.preventDefault();
-    const newItem = document.createElement("li");
-    newItem.innerText = evt.target["new-todo"].value;
-    listedItem.appendChild(newItem);
-})
+
+
+
+
+
+
+
 
 
 
